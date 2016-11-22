df1f1391909a4e2f5342c3a8cce1aa4cd8c0a4be
花儿黑马 <826051996@qq.com>
2016-11-22 16:05:17 +0800

    修改代码格式
    刚才从sublime粘过来的，有格式错误

--- purifier.py
+++ purifier.py
@@ -1,4 +1,3 @@
-#!/usr/bin
 import socket
 import time
 import mraa
@@ -7,32 +6,35 @@ import mraa
 HOST = '144.214.146.211'
 PORT = 4202
 
+
 def connect():
-	while True:
-		try:
-		    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		    s.connect((HOST, PORT))
-		    return s
-		except socket.error:
-	    	print 'socket error'
-	        time.sleep(2)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST, PORT))
+            return s
+        except socket.error:
+            print 'socket error'
+            time.sleep(2)
+
 
 def get_u():
-	u=mraa.Uart(0)
-	u.setBaudRate(115200)
-	u.setMode(8, mraa.UART_PARITY_NONE, 1)
-	return u
+    u=mraa.Uart(0)
+    u.setBaudRate(115200)
+    u.setMode(8, mraa.UART_PARITY_NONE, 1)
+    return u
+
 
 def read_and_send(s, u):
-	while True:
-		try:
-			data_all = u.readStr(100)
-		    s.send(data_all)
-		except socket.error:
-			s = connect()
-			
+    while True:
+        try:
+            data_all = u.readStr(100)
+            s.send(data_all)
+        except:
+            s = connect()
+
 
 if __name__ == "__main__":
-	s = connect()
-	u = get_u()
-	read_and_send(s, u)
+    s = connect()
+    u = get_u()
+    read_and_send(s, u)
